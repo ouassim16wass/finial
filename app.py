7c1764a50134789b4843b8ef24f1a48aed149167
wassim megrad <94735823+ouassim16wass@users.noreply.github.com>
2025-02-12 14:32:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 
-if _name_ ='__name__':
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)

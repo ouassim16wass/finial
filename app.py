8fc8e751818caa261fa2d1bab4cdc17b36808c76
wassim megrad <94735823+ouassim16wass@users.noreply.github.com>
2025-02-12 14:27:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 import pandas as pd
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # 🔹 Configuration de la base PostgreSQL
 DB_USER = "myuser"  # Remplace par ton utilisateur PostgreSQL
@@ -70,7 +70,7 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 
-if _name_ == 'app = Flask(__name__):
+if _name_ ='__name__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
